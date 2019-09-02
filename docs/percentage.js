4adf0048f63cb6227fdabad2633ffd10593ed4f5
epikjjh <epikjjh@gmail.com>
2019-09-02 04:10:24 +0000

    fix progress bar error(2)
    Signed-off-by: epikjjh <epikjjh@gmail.com>

--- docs/percentage.js
+++ docs/percentage.js
@@ -2,18 +2,23 @@ const finishDate = new Date("Mar 12, 2020 00:00:00").getTime();
 const startDate = new Date("April 23, 2018 00:00:00").getTime();
 const duration = finishDate - startDate;
 const percent = document.querySelector(".percentage");
+const plot = document.querySelector(".bgPlot");
 
 function handleTransition(){
  	percent.style.fontSize = "70%";
 }
 
-function init(){
+function handleClick(){
 	const today = new Date().getTime();
 	const diff = today - startDate;
 	const ratio = (diff/duration*100).toFixed(1);
 	const barRatio = (ratio*0.98).toFixed(1);
-	percent.addEventListener("transitionend", handleTransition);
-	percent.style.width = `${barRatio}%`;
+  	percent.style.width = `${barRatio}%`;
 	percent.innerText = `${ratio}%`;
+}
+
+function init(){
+ 	percent.addEventListener("transitionend", handleTransition);
+ 	plot.addEventListener("click", handleClick);
 }
 init();
