793903a39d5074f1b022515c192a4fcb2045c218
epikjjh <epikjjh@gmail.com>
2019-09-02 02:33:18 +0000

    add progress bar
    Signed-off-by: epikjjh <epikjjh@gmail.com>

---
+++ docs/percentage.js
@@ -0,0 +1,19 @@
+const targetDate = new Date("Mar 12, 2020 00:00:00").getTime();
+const startDate = new Date("April 23, 2018 00:00:00").getTime();
+const duration = targetDate - startDate;
+const percent = document.querySelector(".percentage");
+
+function handleTransition(){
+  percent.style.fontSize = "70%";
+}
+
+function init(){
+  const today = new Date().getTime();
+  const diff = today - startDate;
+  const ratio = (diff/duration*100).toFixed(1);
+  const barRatio = (ratio*0.98).toFixed(1);
+  percent.style.width = `${barRatio}%`;
+  percent.innerText = `${ratio}%`;
+  percent.addEventListener("transitionend", handleTransition);
+}
+init();
